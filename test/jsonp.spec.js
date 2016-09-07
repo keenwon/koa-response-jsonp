710d69627a17cf452e0add18edd70467f75c6d2f
keenwon <semanwmj@yeah.net>
2016-09-07 15:57:09 +0800

    添加测试用例

---
+++ test/jsonp.spec.js
@@ -0,0 +1,76 @@
+'use strict';
+
+const koa = require('koa');
+const fetch = require('node-fetch');
+const jsonp = require('../lib/jsonp');
+
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+chai.should();
+chai.use(chaiAsPromised);
+
+describe('koa-json测试', function () {
+
+    afterEach(function () {
+        this.server && this.server.close();
+    });
+
+    it('不传入app', function () {
+        (function () {
+            jsonp();
+        }).should.throw('app is must be an instance of koa');
+    });
+
+    it('传入错误参数', function () {
+        function Test() {
+        }
+
+        (function () {
+            jsonp(new Test());
+        }).should.throw('app is must be an instance of koa');
+    });
+
+    it('测试返回数据', function () {
+        const app = koa();
+
+        jsonp(app);
+
+        app.use(function *() {
+            return this.jsonp({
+                success: true
+            });
+        });
+
+        this.server = app.listen(8888);
+
+        return fetch('http://localhost:8888?callback=fn')
+            .then(response => response.text())
+            .should
+            .eventually
+            .equal('fn({"success":true})');
+    });
+
+    it('测试返回类型', function (done) {
+        const app = koa();
+
+        jsonp(app);
+
+        app.use(function *() {
+            return this.jsonp({
+                success: true
+            });
+        });
+
+        this.server = app.listen(8888);
+
+        fetch('http://localhost:8888?callback=fn')
+            .then(response => {
+                response.headers.get('content-type')
+                    .should
+                    .be
+                    .include('application/json');
+                done();
+            });
+    });
+
+});
